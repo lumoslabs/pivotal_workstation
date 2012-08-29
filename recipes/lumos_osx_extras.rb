4ece6668f9622e2a7f0501a21c3b7b7d2e5cedc8
JaMile Jackson <jamile@lumoslabs.com>
2012-08-28 18:05:24 -0700

    create new recipes and recipe roles for lumos dev workstation

---
+++ recipes/lumos_osx_extras.rb
@@ -0,0 +1,4 @@
+include_recipe "pivotal_workstation::github_for_mac"
+include_recipe "pivotal_workstation::firefox"
+include_recipe "pivotal_workstation::dropbox"
+include_recipe "pivotal_workstation::virtualbox"
