5b81d3e535b1c05505bb31e5bea523f4c0cb2334
JaMile Jackson <jamile@lumoslabs.com>
2012-08-29 11:56:35 -0700

    remove recipes from lumos_osx_dev that are included in vim recipe for lumos_osx_basic

--- recipes/lumos_osx_basic.rb
+++ recipes/lumos_osx_basic.rb
@@ -1,6 +1,5 @@
 include_recipe "pivotal_workstation::java"
 include_recipe "pivotal_workstation::iterm2"
-include_recipe "pivotal_workstation::vim"
 include_recipe "pivotal_workstation::textmate"
 include_recipe "pivotal_workstation::textmate_bundles"
 include_recipe "pivotal_workstation::textmate_preferences"
