cb5a7048fdb47be2b226b987cb132f13c74657a5
JaMile Jackson <jamile@lumoslabs.com>
2012-09-06 15:12:24 -0700

    more troubleshooting

--- recipes/set_finder_show_all_hd_on_desktop.rb
+++ recipes/set_finder_show_all_hd_on_desktop.rb
@@ -1,20 +1,17 @@
 pivotal_workstation_defaults "show hard drive on desktop" do
   domain 'com.apple.finder'
   key 'ShowHardDrivesOnDesktop'
-  boolean false
-  boolean true
+  integer 1
 end
 
 pivotal_workstation_defaults "show external hard drives on desktop" do
   domain 'com.apple.finder'
   key 'ShowExternalHardDrivesOnDesktop'
-  boolean false
   boolean true
 end
 
 pivotal_workstation_defaults "show removable media on desktop" do
   domain 'com.apple.finder'
   key 'ShowRemovableMediaOnDesktop'
-  boolean false
   boolean true
 end
