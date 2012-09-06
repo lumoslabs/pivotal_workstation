cdc0a7707baa7ddb858084e939da7fe4af9dea0d
JaMile Jackson <jamile@lumoslabs.com>
2012-09-06 15:14:16 -0700

    troubleshoot display locking

--- recipes/set_screensaver_preferences.rb
+++ recipes/set_screensaver_preferences.rb
@@ -10,14 +10,14 @@ end
 #   float 60
 # end
 
-# plist_dir = ENV['HOME'] + "/Library/Preferences/ByHost"
-# Dir["#{plist_dir}/com.apple.screensaver.*.plist"].each do |file|
-#   pivotal_workstation_defaults "set screensaver timeout" do
-#     domain file.chomp('plist')
-#     key 'idleTime'
-#     integer 600
-#   end
-# end
+plist_dir = ENV['HOME'] + "/Library/Preferences/ByHost"
+Dir["#{plist_dir}/com.apple.screensaver.*.plist"].each do |file|
+  pivotal_workstation_defaults "set screensaver timeout" do
+    domain file.chomp('plist')
+    key 'idleTime'
+    integer 600
+  end
+end
 
 execute "set display, disk and computer sleep times" do
   command "pmset -a displaysleep 1 disksleep 0 sleep 0"
