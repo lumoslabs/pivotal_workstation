8b8c6978832207273907c40d3fdee57dd1bc6835
JaMile Jackson <jamile@lumoslabs.com>
2012-09-06 14:27:42 -0700

    added finder preferences
    sleep machine and require password
    show ext hd on desktop

--- recipes/set_screensaver_preferences.rb
+++ recipes/set_screensaver_preferences.rb
@@ -4,21 +4,21 @@ pivotal_workstation_defaults "ask for password when screen is locked" do
   integer 1
 end
 
-pivotal_workstation_defaults "wait 60 seconds between screensaver & lock" do
-  domain 'com.apple.screensaver'
-  key 'askForPasswordDelay'
-  float 60
-end
+# pivotal_workstation_defaults "wait 60 seconds between screensaver & lock" do
+#   domain 'com.apple.screensaver'
+#   key 'askForPasswordDelay'
+#   float 60
+# end
 
-plist_dir = ENV['HOME'] + "/Library/Preferences/ByHost"
-Dir["#{plist_dir}/com.apple.screensaver.*.plist"].each do |file|
-  pivotal_workstation_defaults "set screensaver timeout" do
-    domain file.chomp('plist')
-    key 'idleTime'
-    integer 600
-  end
-end
+# plist_dir = ENV['HOME'] + "/Library/Preferences/ByHost"
+# Dir["#{plist_dir}/com.apple.screensaver.*.plist"].each do |file|
+#   pivotal_workstation_defaults "set screensaver timeout" do
+#     domain file.chomp('plist')
+#     key 'idleTime'
+#     integer 600
+#   end
+# end
 
 execute "set display, disk and computer sleep times" do
-  command "pmset -a displaysleep 20 disksleep 15 sleep 0"
+  command "pmset -a displaysleep 2 disksleep 0 sleep 0"
 end
