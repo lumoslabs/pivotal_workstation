62b72ecd3e42535c261c4b147ae1a3d3b5776227
JaMile Jackson <jamile@lumoslabs.com>
2012-09-26 15:50:45 -0700

    remove include_java recipe and added comma for default_rubygems

--- attributes/rvm.rb
+++ attributes/rvm.rb
@@ -7,6 +7,6 @@ node.default["rvm"]= {
     #"ruby-1.9.2-p290" => { :env => "CC=gcc-4.2" },
     "ruby-1.9.3-p125" => { :env => "CC=gcc-4.2" },
   },
-  "default_ruby" => "ruby-1.9.3-p125"
+  "default_ruby" => "ruby-1.9.3-p125",
   "default_rubygems" => "1.8.10"
 }
